6c62c9a20e3b5fc332b6b2d71a300de0ecd5231d
Emily Pena <emilyapena@gmail.com>
2020-05-26 00:33:36 -0600

    stubbed modals

---
+++ src/modals/LWABuilders.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Portfolio.css";
+
+const LWABuilders = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const Modal = () => {
+    return (
+      <>
+        <div style={styles.modal}>
+          <div style={styles.modalContent}>
+            <button
+              style={styles.modalExit}
+              onClick={() => setShowForm(!showForm)}
+            >
+              {showForm ? "x" : "View"}
+            </button>
+            <div style={styles.modalHeader}>
+              <h1 style={styles.h1}>Batters Up Menu Design</h1>
+              <a href="https://www.emilypena.me" style={styles.circle}>
+                <FontAwesomeIcon icon={faGithub} size="lg" />
+              </a>
+              <a href="https://www.emilypena.me" style={styles.circle}>
+                <FontAwesomeIcon icon={faEye} />
+              </a>
+            </div>
+            <div style={styles.modalImg}>
+              <img
+                alt="Test"
+                src="https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/BattersUp1_cmrx5k.jpg"
+              />
+              <img
+                alt="Test"
+                src="https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/BattersUp2_tlthiu.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="column">
+      {showForm && <Modal toggleForm={setShowForm} />}
+      <img
+        onClick={() => setShowForm(!showForm)}
+        alt="Test"
+        src="https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/BattersUp2_tlthiu.jpg"
+      />
+    </div>
+  );
+};
+
+export default LWABuilders;
+
+const styles = {
+  headerContainer: {
+    backgroundColor: "#232323",
+    width: "auto",
+    height: "auto",
+    color: "#00FFD1",
+    textAlign: "center",
+    padding: "50px",
+    paddingTop: "100px",
+    marginTop: "1.5em",
+  },
+
+  modal: {
+    position: "fixed",
+    zIndex: "1",
+    left: "0",
+    top: "0",
+    width: "100%",
+    height: "100%",
+    overflow: "auto",
+    backgroundColor: "rgba(0,0,0,0.8)",
+  },
+
+  modalContent: {
+    backgroundColor: "#232323",
+    margin: "15% auto",
+    padding: "2em",
+    width: "60%",
+  },
+
+  modalHeader: {
+    paddingBottom: "4em",
+    textAlign: "center",
+    color: "#00FFD1",
+  },
+
+  modalImg: {
+    width: "auto",
+    border: "none",
+  },
+
+  modalExit: {
+    border: "1px solid #ADADAD",
+    borderRadius: "50%",
+    width: "30px",
+    padding: ".75em",
+    color: "#ADADAD",
+    cursor: "pointer",
+    fontSize: "10px",
+  },
+
+  h1: {
+    color: "#00FFD1",
+  },
+
+  p2: {
+    color: "#ADADAD",
+    lineHeight: "23px",
+  },
+
+  circle: {
+    marginRight: "1em",
+    border: "1px solid #00FFD1",
+    borderRadius: "50%",
+    width: "20px",
+    padding: ".75em",
+    textAlign: "center",
+    color: "#00FFD1",
+  },
+};
