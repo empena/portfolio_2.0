f0611d245ef0b76fe4fd9340cafab9c28fb1a205
Emily Pena <emilyapena@gmail.com>
2020-05-25 20:31:40 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import NoMatch from "./components/NoMatch";
+import Design from "./components/Design";
+import Code from "./components/Code";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Fragment>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/design" component={Design} />
+        <Route exact path="/code" component={Code} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
-  );
-}
+  </Fragment>
+);
 
 export default App;
