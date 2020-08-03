87270f6ab8a2ed4563af0b5280583763dc529ec2
Emily Pena <emilyapena@gmail.com>
2020-08-02 21:57:26 -0600

    badge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,8 @@ const Contact = () => {
           <div className="contact">
             <h1 className="h1">Contact</h1>
             <p className="p2">
-              If you have a request or question don’t hesitate to contact me with the form below.
+              If you have a request or question don’t hesitate to contact me
+              with the form below.
             </p>
             <ContactForm />
           </div>
@@ -29,6 +30,16 @@ const Contact = () => {
                 <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
               </a>
             </div>
+            <br />
+            <br />
+            <a href="https://www.youracclaim.com/badges/b8076ad3-3d60-47b2-b439-3d60fbcb26e7/linked_in_profile">
+              <img
+                alt="badge"
+                src="https://res.cloudinary.com/dvwwzmsmn/image/upload/v1596426577/Portfolio/badge_o6wbu1.png"
+                width="80"
+                height="80"
+              />
+            </a>
           </div>
         </div>
       </div>
